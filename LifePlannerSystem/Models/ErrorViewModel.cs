22b458f7eaa52b1054c923494e044851cb644b48
Art <artpreniqi14@gmail.com>
2025-12-07 15:14:17 +0100

    Initial project setup

---
+++ LifePlannerSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace LifePlannerSystem.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
